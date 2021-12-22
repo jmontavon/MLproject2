bb762030753b62c5045a58256f04aba46113f956
Alexandre Coudray <alexandre.coudray@epfl.ch>
2021-12-22 16:08:44 +0100

    README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# MLproject2
+### MLproject2
 Project for the EPFL Machine Learning course done in the scope of ML4Science.
+
+## Installation of Python Env
+
+First install the environment project-env
+`python3 -m venv project-env` 
+
+Connect yourself to this newly created env
+`source project-env/bin/activate`
+
+Then install all requirements : 
+`pip install -r utils/requirements.txt`
+
+Enjoy !
+
+Tested on MACOSX Catalina. Tensorflow-GPU for DELTA was not tested. 
+
+## Launching the training of DELTA models 
+
+All the path to the training dataset folder and to the model file are in config.py. All the path should be correct. It needs the folder data/ to be in the root of the project folder.
+
+To launch segmentation training : 
+
+`python3 DELTA_train_segmentation.py`
+
+To launch cell tracking training :
+
+`python3 DELTA_train_tracking.py`
+
+# Prediction using already built DELTA models 
+
+All the path to the training dataset folder and to the model file are in config.py. All the path should be correct. It needs the folder data/ to be in the root of the project folder.
+
+To launch segmentation training : 
+
+`python3 DELTA_train_segmentation.py`
+
+# 
--- config.py
+++ config.py
@@ -1,52 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb  5 15:42:05 2021
-bla
+
 @author: jblugagne
+simplified by A.Coudray
 """
 
-# Download latest models first:
-# https://drive.google.com/drive/folders/1nTRVo0rPP9CR9F6WUunVXSXrLNMT_zCP
+models = ['segmentation', 'tracking']
+model_file_seg = 'data/models/model_segmentation.hdf5'
+model_file_track = 'data/models/model_tracking.hdf5'
 
-#presets = 'mothermachine' # 'mothermachine' or '2D'
-presets = '2D'
+target_size_seg = (512, 512)
+target_size_track = (256, 256)
 
-if presets == 'mothermachine':
-    models = ['rois', 'segmentation', 'tracking']
-    model_file_rois = 'data/models/chambers_id_tessiechamp.hdf5'
-    model_file_seg = 'data/models/unet_moma_seg_multisets.hdf5'
-    model_file_track = 'data/models/unet_moma_track_v2.hdf5'
-    target_size_rois = (512, 512)
-    target_size_seg = (256, 32)
-    target_size_track = target_size_seg
-    
-    # Training sets:
-    training_set_rois = 'data/training/chambers_seg_set/train'
-    training_set_seg = 'data/training/segmentation_set/train_multisets/'
-    training_set_track = 'data/training/tracking_set/train_multisets'
-    
-    # Pipeline parameters:
-    rotation_correction = True # Whether to perform automated image rotation correction
-    drift_correction = True # Whether to perform image drift correction
-    crop_windows = False # Whether to crop out windows for segmentation and tracking
-    min_roi_area = 500 # Minimum size of ROIs/chambers in pixels for area filtering (0 to +Inf)
-    
-elif presets == '2D':
-    models = ['segmentation', 'tracking']
-    model_file_seg = '/Users/acoudray/Desktop/tmp_samba/phd_courses/machine_learning/project2/delta_dev_branch/delta/training_curated_seg_1/model_seg_CustomWeights_sig50_q085_pow5.hdf5'
-    model_file_track = '/Users/acoudray/Desktop/tmp_samba/phd_courses/machine_learning/project2/delta_dev_branch/delta/training_tracking_n3/model_track.hdf5'
+# Training sets:
+training_set_seg = 'data/training_segmentation'
+training_set_track = 'data/training_tracking'
 
-    target_size_seg = (512, 512)
-    target_size_track = (256, 256)
-    
-    # Training sets:
-    training_set_seg = '/Users/acoudray/Desktop/tmp_samba/phd_courses/machine_learning/project2/delta_dev_branch/delta/training_curated_seg_1'
-    training_set_track = '/Users/acoudray/Desktop/tmp_samba/phd_courses/machine_learning/project2/delta_dev_branch/delta/training_tracking_n3'
-    
-    # Pipeline parameters:
-    rotation_correction = False # Whether to perform automated image rotation correction
-    drift_correction = False # Whether to perform image drift correction
-    crop_windows = True # Whether to crop out windows for segmentation and tracking
+# Pipeline parameters:
+rotation_correction = False # Whether to perform automated image rotation correction
+drift_correction = False # Whether to perform image drift correction
+crop_windows = True # Whether to crop out windows for segmentation and tracking
 
 # Other pipeline parameters:
 whole_frame_drift = False # Whether to use the whole frame for drift correction
